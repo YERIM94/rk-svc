5907e07888a86b89d6c9640dd6ce8b10bc669557
yerim94 <721yerim@gmail.com>
2023-03-26 18:54:01 +0900

    Initial commit

---
+++ src/main/java/com/rimkim/rksvc/RksvcApplication.java
@@ -0,0 +1,12 @@
+package com.rimkim.rksvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RksvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RksvcApplication.class, args);
+	}
+}
